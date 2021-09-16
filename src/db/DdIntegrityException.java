72076cd355d8121513d0ac45ec1a2eeaf6906f3e
Lucas Ap Santos <lucasapdsantos@gmail.com>
2021-09-15 21:33:13 -0300

    Adding database access

---
+++ src/db/DdIntegrityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DdIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DdIntegrityException(String msg) {
+		super(msg);
+	}
+
+}
